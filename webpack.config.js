e62027b1afe59aa5038f62362c4291d3345c3703
santos1205 <mariosantos1205@gmail.com>
2018-11-06 22:51:28 -0200

    xxxx

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         }),
-        //new webpack.optimize.UglifyJsPlugin()
+        new webpack.optimize.UglifyJsPlugin()
     ],
     module: {
         loaders: [{
