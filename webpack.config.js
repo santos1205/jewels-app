a688a4e7380ca74dd4e6b9b26d799ed8968164c4
santos1205 <mariosantos1205@gmail.com>
2019-02-02 10:57:56 -0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,8 @@ module.exports = {
             exclude: /node_modules/,
             query: {
                 presets:['es2015', 'react'],                
-                plugins:['transform-object-rest-spread']
+                plugins:['transform-object-rest-spread'],
+                plugins: ['transform-class-properties']
             }
         }, {
             test: /\.css$/,
